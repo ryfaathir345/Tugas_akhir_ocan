67803c693bca72292c085bb9ab70f662d63808bd
ryfaathir345 <miko35292@gmail.com>
2023-05-05 04:55:15 +0700

    Tugas Akhir Ocan

---
+++ index.php
@@ -0,0 +1,309 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>LAPANG</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="wrapper">
+      <!-- end loader -->
+      <div class="sidebar">
+         <!-- Sidebar  -->
+         <nav id="sidebar">
+            <div id="dismiss">
+               <i class="fa fa-arrow-left"></i>
+            </div>
+            <ul class="list-unstyled components">
+               <li><a href="#">Home</a> </li>
+               <li><a href="#about">About </a> </li>
+               <li><a href="#work">Work</a> </li>
+               <li><a href="#contact">Contact </a> </li>
+            </ul>
+         </nav>
+      </div>
+      <div id="content">
+         <!-- header -->
+         <header>
+            <!-- header inner -->
+            <div class="header">
+               <div class="container-fluid">
+                  <div class="row">
+                     <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                        <div class="full">
+                           <div class="center-desk">
+                              <div class="logo">
+                                 <a href="index.html"><img src="images/logo.png" alt="#" /></a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                        <ul class="btn">
+                           <li class="down_btn"><a href="Register.php">Sign</a></li>
+                           <li><a href="Login.php">Sign in</a></li>
+                           <li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
+                           <li><button type="button" id="sidebarCollapse">
+                              <img src="images/menu_icon.png" alt="#" />
+                              </button>
+                           </li>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </header>
+         <!-- end header inner -->
+         <!-- end header -->
+         <!-- banner -->
+         <div id="myCarousel" class="carousel slide banner_main" data-ride="carousel">
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <div class="container-fluid">
+                     <div class="carousel-caption">
+                        <div class="row">
+                           <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+                              <div class="text-bg">
+                                 <h1>LAPANG</h1>
+                                 <p>Tempat penjualan laptop paling besar seantero Malang</p>
+                                 <a class="read_more" href="#about">Read more</a>
+                              </div>
+                           </div>
+                           <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
+                              <div class="images_box">
+                                 <figure><img src="images/img2.png"></figure>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+            <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
+            </a>
+            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+            <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </a>
+         </div>
+         <!-- end banner -->
+         <!-- about -->
+         <div id="about"  class="about">
+            <div class="container-fluid">
+               <div class="row d_flex">
+                  <div class="col-md-5">
+                     <div class="about_img">
+                        <figure><img src="images/about_img.jpg" alt="#"/></figure>
+                     </div>
+                  </div>
+                  <div class="col-md-7">
+                     <div class="titlepage">
+                        <h2>About <span class="blu">LAPANG</span></h2>
+                        <p>Tempat penjualan laptop paling besar seantero Malang yang telah berdiri sejak tahun 2022 dan telah berkembang pesat sejak pertamakali peresmiannya,setelah menjadi toko laptop besar di malang,lapang memiliki banyak kerja sama dengan perusahaan perusahaan ternama seperti ASUS,MSI dan juga lainnya </p>
+                        <a class="read_more" href="#work">Read More</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- end about -->
+         <!-- choose  section -->
+         <div class="choose ">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-12">
+                     <div class="titlepage">
+                        <h2>Must<span class="blu"> LAPANG</span></h2>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- end choose  section -->
+         <!-- work -->
+         <div id="work"  class="work">
+            <div class="container-fluid">
+               <div class="row d_flex">
+                  <div class="col-md-7">
+                     <div class="titlepage">
+                        <h2>How To <span class="blu">Works it</span></h2>
+                        <p>LAPANG bekerja sama dengan brand laptop ternama untuk menyediakan barang yang berkualitas serta dapat membantu berbagai macam kegiatan costumers </p>
+                        <a class="read_more" href="#contact">Read More</a>
+                     </div>
+                  </div>
+                  <div class="col-md-5">
+                     <div class="work_img">
+                        <figure><img src="images/work_img.jpg" alt="#"/></figure>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- end work -->
+         <!-- request -->
+         <div id="contact" class="request">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-12">
+                     <div class="titlepage">
+                        <center><h2>Request <span class="white">A call Back?</span></h2></center>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- end request -->
+         <!--  footer -->
+         <footer>
+            <div id="contact"  class="contact">
+               <div class="footer">
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                           <div class="row">
+                              <div class="col-md-8 col-sm-6">
+                                 <div class="address">
+                                    <h3>Address </h3>
+                                 </div>
+                                 <ul class="location_icon">
+                                 <li>Jl. Pelabuhan Bakahuni, Bakalankrajan, Kec. Sukun, Kota Malang, Jawa Timur 65148</li>
+                                 </ul>
+                              </div>
+                              <div class="col-md-4 col-sm-6">
+                                 <div class="address">
+                                    <h3>Links</h3>
+                                    <ul class="Menu_footer">
+                                       <li class="active"> <a href="#">Home</a> </li>
+                                       <li><a href="#about">About </a> </li>
+                                       <li><a href="#work">Work</a> </li>
+                                       <li><a href="#contact">Contact </a> </li>
+                                    </ul>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                        <div class="row">
+                              <div class="col-md-5 col-sm-6">
+                                 <div class="address">
+                                    <h3>Follow Us</h3>
+                                 </div>
+                                 <ul class="social_icon">
+                                    <li><a href="https://instagram.com/ocanokan?igshid=NTE5MzUyOTU=">@okanocan<i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                                 </ul>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="copyright">
+                     <div class="container">
+                        <div class="row">
+                           <div class="col-md-12">
+                              <p>No Copyright</p>
+                           </div>
+                        </div>
+                     </div>
+               </div>
+            </div>
+         </footer>
+         <!-- end footer -->
+      </div>
+      <div class="overlay"></div>
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <script type="text/javascript">
+         $(document).ready(function() {
+             $("#sidebar").mCustomScrollbar({
+                 theme: "minimal"
+             });
+         
+             $('#dismiss, .overlay').on('click', function() {
+                 $('#sidebar').removeClass('active');
+                 $('.overlay').removeClass('active');
+             });
+         
+             $('#sidebarCollapse').on('click', function() {
+                 $('#sidebar').addClass('active');
+                 $('.overlay').addClass('active');
+                 $('.collapse.in').toggleClass('in');
+                 $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+             });
+         });
+      </script>
+      <script>
+         $(document).ready(function() {
+             $(".fancybox").fancybox({
+                 openEffect: "none",
+                 closeEffect: "none"
+             });
+         
+             $(".zoom").hover(function() {
+         
+                 $(this).addClass('transition');
+             }, function() {
+         
+                 $(this).removeClass('transition');
+             });
+         });
+      </script>
+      <script>
+         // This example adds a marker to indicate the position of Bondi Beach in Sydney,
+         // Australia.
+         function initMap() {
+           var map = new google.maps.Map(document.getElementById('map'), {
+             zoom: 11,
+             center: {
+               lat: 40.645037,
+               lng: -73.880224
+             },
+           });
+         
+           var image = 'images/maps-and-flags.png';
+           var beachMarker = new google.maps.Marker({
+             position: {
+               lat: 40.645037,
+               lng: -73.880224
+             },
+             map: map,
+             icon: image
+           });
+         }
+      </script>
+   </body>
+</html>
+
---
+++ login.php
@@ -0,0 +1,262 @@
+<?php
+
+include 'DB.php';
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    // username and password sent from form 
+    
+    $myusername = mysqli_real_escape_string($conn,$_POST['namauser']);
+    $mypassword = mysqli_real_escape_string($conn,$_POST['kunci']); 
+    
+    $sql = "SELECT * FROM register WHERE name = '$myusername' and password = '$mypassword'";
+    $result = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    // $active = $row['active'];
+    
+    $count = mysqli_num_rows($result);
+    
+    // If result matched $myusername and $mypassword, table row must be 1 row
+      
+    if($count == 1) {
+    //    session_register("myusername");
+       $_SESSION['login_user'] = $myusername;
+       echo "<script type='text/javascript'> alert('Login berhasil'); </script> ";
+       header("location: http://localhost/tugasakhir/index.php");
+       exit;
+
+    }elseif(empty($myusername)  || empty($mypassword)) {
+        $error = "please fill all input";
+        echo "<script type='text/javascript'> alert('please fill all input'); </script> ";
+    }else{
+        $error = "Your Login Password is invalid";
+       echo "<script type='text/javascript'> alert('password salah'); </script> ";
+    }
+ }
+ 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+</head>
+<style>
+    body,
+.signin {
+  background: url(images/keyboard-buttons-letters-1600x1200.jpg);
+  font-family: 'Montserrat', sans-serif;
+  color: #fff;
+  font-size: 14px;
+  letter-spacing: 1px;
+}
+
+.login {
+  position: relative;
+  height: 560px;
+  width: 405px;
+  margin: auto;
+  padding: 60px 60px;
+  background: url(https://picsum.photos/id/1004/5616/3744) no-repeat   center center #505050;   
+  background-size: cover;
+  box-shadow: 0px 30px 60px -5px #000;
+}
+
+form {
+  padding-top: 80px;
+}
+
+.active {
+  border-bottom: 2px solid #1161ed;
+}
+
+.nonactive {
+  color: rgba(255, 255, 255, 0.2);
+}
+
+h2 {
+  padding-left: 12px;
+  font-size: 22px;
+  text-transform: uppercase;
+  padding-bottom: 5px;
+  letter-spacing: 2px;
+  display: inline-block;
+  font-weight: 100;
+}
+
+h2:first-child {
+  padding-left: 0px;
+}
+
+span {
+  text-transform: uppercase;
+  font-size: 12px;
+  opacity: 0.4; 
+  display: inline-block;
+  position: relative;
+  top: -65px;
+  transition: all 0.5s ease-in-out;
+}
+
+.text {
+  border: none;
+  width: 89%;
+  padding: 10px 20px;
+  display: block;
+  height: 15px;
+  border-radius: 20px;
+  background: rgba(255, 255, 255, 0.1);
+  border: 2px solid rgba(255, 255, 255, 0);
+  overflow: hidden;
+  margin-top: 15px;
+  transition: all 0.5s ease-in-out;
+}
+
+.text:focus {
+  outline: 0;
+  border: 2px solid rgba(255, 255, 255, 0.5);
+  border-radius: 20px;
+  background: rgba(0, 0, 0, 0);
+}
+
+.text:focus + span {
+  opacity: 0.6;
+}
+
+input[type="text"],
+input[type="password"] {
+  font-family: 'Montserrat', sans-serif;
+  color: #fff;
+}
+
+
+
+input {
+  display: inline-block;
+  padding-top: 20px;
+  font-size: 14px;
+}
+
+h2,
+span,
+.custom-checkbox {
+  margin-left: 20px;
+}
+
+.custom-checkbox {
+  -webkit-appearance: none;
+  background-color: rgba(255, 255, 255, 0.1);
+  padding: 8px;
+  border-radius: 2px;
+  display: inline-block;
+  position: relative;
+  top: 6px;
+}
+
+.custom-checkbox:checked {
+  background-color: rgba(17, 97, 237, 1);
+}
+
+.custom-checkbox:checked:after {
+  content: '\2714';
+  font-size: 10px;
+  position: absolute;
+  top: 1px;
+  left: 4px;
+  color: #fff;
+}
+
+.custom-checkbox:focus {
+  outline: none;
+}
+
+label {
+  display: inline-block;
+  padding-top: 10px;
+  padding-left: 5px;
+}
+
+.signin {
+  background-color: #1161ed;
+  color: #FFF;
+  width: 100%;
+  padding: 10px 20px;
+  display: block;
+  height: 39px;
+  border-radius: 20px;
+  margin-top: 30px;
+  transition: all 0.5s ease-in-out;
+  border: none;
+  text-transform: uppercase;
+}
+
+.signin:hover {
+  background: #4082f5;
+  box-shadow: 0px 4px 35px -5px #4082f5;
+  cursor: pointer;
+}
+
+.signin:focus {
+  outline: none;
+}
+
+hr {
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  top: 85px;
+  position: relative;
+}
+
+a {
+  text-decoration: none;
+  color: #fff;
+}
+</style>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<body>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+
+<div class="login">
+    <form action="" method="post" >
+  <h2 class="active">sign in</h2><h2><a class="active"href="index.php">lapang</a></h2> 
+  
+   
+    
+
+    <input type="text" class="text" name="namauser">
+     <span>username/email</span>
+
+    <br>
+    
+    <br>
+
+    <input type="password" class="text" name="kunci">
+    <span>password</span>
+    <br>
+    <input type="submit" class="signin" onclick="contoh()" value="submit">
+    <!-- <center><h2><strong><a href="index.html">LAPANG</a></strong></h2></center> -->
+    <center><h2><strong><a href="Registrate.php">Register</a></strong></h2></center>
+  </form>
+
+</div>
+</body>
+<!-- https://script.google.com/macros/s/AKfycbztUtIgjLNSYvwyxh3i-ia-4RRGALlpjm8XRm7HJIKGTvziDY2AaX9DdeXB8RKPVNQwXw/exec -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous">
+  </script>
+  <!-- <script>
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbztUtIgjLNSYvwyxh3i-ia-4RRGALlpjm8XRm7HJIKGTvziDY2AaX9DdeXB8RKPVNQwXw/exec'
+    const form = document.forms['submit-to-google-sheet']
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+    })
+  </script> -->
+
+</html>
